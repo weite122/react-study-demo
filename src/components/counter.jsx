0e2f7a466a2b6561aa9bf0bb703eaa684e73720f
weite122 <949193531@qq.com>
2019-08-22 20:23:43 +0800

    [A]first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: 'https://picsum.photos/200'
+  };
+  render() {
+    return (
+      <React.Fragment>
+          <img src={this.state.imageUrl} alt=""/>
+        <span>{this.formatCount()}</span>
+        <button>Increment</button>
+      </React.Fragment>
+    );
+  }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
+  }
+}
+
+export default Counter;
