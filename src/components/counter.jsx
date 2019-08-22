e6f29e55d0d9fef7f51fdab61072e95ca7c91b09
weite122 <949193531@qq.com>
2019-08-22 20:34:46 +0800

    [A]设置class属性

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,21 +2,23 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    imageUrl: 'https://picsum.photos/200'
+    count: 0
   };
+  styles = {
+      fontSize: 30,
+      fontWeight: 'bold'
+  }
   render() {
     return (
       <React.Fragment>
-          <img src={this.state.imageUrl} alt=""/>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button style={{ fontSize: 20}} className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
