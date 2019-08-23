f85b80db49e405eb4c30051937909366405852e2
weite122 <949193531@qq.com>
2019-08-23 17:49:10 +0800

    [A]父子组件传值

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
   };
 
   handleIncrement = product => {
@@ -12,7 +12,6 @@ class Counter extends Component {
   render() {
     return (
       <div>
-        {/* {this.props.children} */}
         <h4>{this.props.id}</h4>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
@@ -21,6 +20,7 @@ class Counter extends Component {
         >
           Increment
         </button>
+        <button onClick={() =>this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
     );
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -10,13 +10,20 @@ class Counters extends Component {
       { id: 4, value: 0 }
     ]
   };
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <div>
         {this.state.counters.map(counter => (
-          <Counter key={counter.id} value={counter.value} id={counter.id}>
-              {/* <h4>Counter #{counter.id}</h4> */}
-          </Counter>
+          <Counter
+            key={counter.id}
+            onDelete={this.handleDelete}
+            counter={counter}
+          />
         ))}
       </div>
     );
