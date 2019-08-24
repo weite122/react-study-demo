63551d7a6d2f532740fcbd858ba65a52faf8ea94
weite122 <949193531@qq.com>
2019-08-24 17:27:07 +0800

    [A]无状态组件+解构赋值简化代码

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,17 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar nav-light bg-light">
-        <a className="navbar-brand" href="#">
-          NavBar{" "}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = ({totalCounters}) => {
+  return (
+    <nav className="navbar nav-light bg-light">
+      <a className="navbar-brand" href="#">
+        NavBar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
