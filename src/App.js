1a301815974e0cb8949ec8e56dea7dfff20aaed1
weite122 <949193531@qq.com>
2019-08-25 00:23:32 +0800

    [A]添加减少按钮

--- src/App.js
+++ src/App.js
@@ -20,6 +20,13 @@ class App extends Component {
     counters[index].value++;
     this.setState({ counters });
   };
+  handleDecrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value--;
+    this.setState({ counters });
+  };
   handleReset = () => {
     const counters = this.state.counters.map(c => {
       c.value = 0;
@@ -39,12 +46,13 @@ class App extends Component {
         <NavBar
           totalCounters={this.state.counters.filter(c => c.value > 0).length}
         />
-        <main className="container">
+        <main>
           <Counters
             counters={this.state.counters}
             onReset={this.handleReset}
             onDelete={this.handleDelete}
             onIncrement={this.handleIncrement}
+            onDecrement={this.handleDecrement}
           />
         </main>
       </React.Fragment>
