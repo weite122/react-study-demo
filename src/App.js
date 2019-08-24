09bee4729259761ef1aee3af65e04cc658d07eb5
weite122 <949193531@qq.com>
2019-08-24 16:57:25 +0800

    [A]上移state和添加navbar功能

--- src/App.js
+++ src/App.js
@@ -4,13 +4,48 @@ import NavBar from "./components/navbar";
 import "./App.css";
 
 class App extends Component {
-  state = {};
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+
+  handleIncrement = counter => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  handleDelete = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <NavBar />
+        <NavBar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}
+        />
         <main className="container">
-          <Counters />
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onDelete={this.handleDelete}
+            onIncrement={this.handleIncrement}
+          />
         </main>
       </React.Fragment>
     );
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from "react";
 
 class NavBar extends Component {
-    render() { 
-        return ( 
-            <nav className="navbar nav-light bg-light">
-                <span className="navbar-brand">NavBar</span>
-            </nav>
-         );
-    }
+  render() {
+    return (
+      <nav className="navbar nav-light bg-light">
+        <a className="navbar-brand" href="#">
+          NavBar{" "}
+          <span className="badge badge-pill badge-secondary">
+            {this.props.totalCounters}
+          </span>
+        </a>
+      </nav>
+    );
+  }
 }
- 
+
 export default NavBar;
