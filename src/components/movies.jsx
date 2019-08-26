6c9bb1e6b747397f49a99f016ab125ca472937a5
weite122 <949193531@qq.com>
2019-08-26 23:09:04 +0800

    [A]defaultProps

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -47,8 +47,6 @@ class Moives extends Component {
         <div className="col-3">
           <ListGroup
             items={this.state.genres}
-            textProperty="name"
-            valueProperty="_id"
             onItemSelect={this.handleGenreSelect}
           />
         </div>
