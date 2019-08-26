2d23fe1d9edd57c0f2f69a9cf2282f6027c08655
weite122 <949193531@qq.com>
2019-08-26 23:52:03 +0800

    [A]实现筛选功能

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -39,9 +39,18 @@ class Moives extends Component {
 
   render() {
     const { length: count } = this.state.movies;
-    const { pageSize, currentPage, movies: allMovies } = this.state;
+    const {
+      pageSize,
+      currentPage,
+      selectedGenre,
+      movies: allMovies
+    } = this.state;
     if (count === 0) return <p>There is no movies in database!</p>;
-    const movies = paginate(allMovies, currentPage, pageSize);
+
+    const filtered = selectedGenre
+      ? allMovies.filter(m => m.genre._id === selectedGenre._id)
+      : allMovies;
+    const movies = paginate(filtered, currentPage, pageSize);
     return (
       <div className="row">
         <div className="col-3">
@@ -52,7 +61,7 @@ class Moives extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {count} movies in the database.</p>
+          <p>Showing {filtered.length} movies in the database.</p>
           <table className="table">
             <thead>
               <tr>
@@ -90,7 +99,7 @@ class Moives extends Component {
             </tbody>
           </table>{" "}
           <Pagination
-            itemsCount={count}
+            itemsCount={filtered.length}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}
