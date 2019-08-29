3b97ea7c0b2e1682297a3a5a5d093231587b9be7
weite122 <949193531@qq.com>
2019-08-30 05:06:56 +0800

    [A]封装表格

---
+++ src/components/common/table.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import TableHeader from "./tableHeader";
+import TableBody from "./tablebody";
+
+const Table = (props) => {
+  const {columns, sortColumn, onSort, data} = this.props
+
+  return (
+    <table className="table">
+      <TableHeader
+        columns={columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      />
+      <TableBody columns={columns} data={data} />
+    </table>
+  );
+};
+
+export default Table;
