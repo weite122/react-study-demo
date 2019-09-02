48378e2ca2549b65a3aae4d9fdde9e255862ade8
weite122 <949193531@qq.com>
2019-09-03 03:28:59 +0800

    [A]注册路由

---
+++ vidly/src/App.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import {Route, Redirect, Switch} from 'react-router-dom'
+import Movies from "./components/movies";
+import NotFound from "./components/NotFound";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import "./App.css";
+
+
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers}/>
+          <Route path="/rentals" component={Rentals}/>
+          <Route path="/not-found" component={NotFound}/>
+          <Redirect from="/" exact to="/movies"/>
+          <Redirect to="/not-found"/>
+        </Switch>
+      </main>
+    );
+  }
+}
+
+export default App;
