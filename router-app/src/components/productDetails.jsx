9fba7d2a15e54d05ca583b999b3dfca3c8d22153
weite122 <949193531@qq.com>
2019-09-03 02:47:27 +0800

    [A]push 和 replace

--- router-app/src/components/productDetails.jsx
+++ router-app/src/components/productDetails.jsx
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 class ProductDetails extends Component {
   handleSave = () => {
     // Navigate to /products
+    this.props.history.push("./products");
+    // push 可以记录浏览历史,replace不能记录，它用于登录页面
   };
 
   render() {
