d62948834cb42424300fa8d06f01695981d88e28
weite122 <949193531@qq.com>
2019-09-03 22:35:44 +0800

    [A]清除input自身的state

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,15 +1,19 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
 
 class LoginForm extends Component {
-  username = React.createRef()
-
-  // componentDidMount() {
-  //   this.username.current.focus()
-  // }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    const username = this.username.current.value
+  state = {
+    account: {
+      username: '',
+      password: ''
+    }
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+  };
+  handleChange = e =>{
+    const account = {...this.state.account}
+    account.username = e.currentTarget.value
+    this.setState({account})
   }
 
   render() {
@@ -17,10 +21,20 @@ class LoginForm extends Component {
       <div>
         <h1>login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group"><label htmlFor="username">Username</label>
-            <input autoFocus ref={this.username} id="username" type="text" className="form-control"/>
+          <div className="form-group">
+            <label htmlFor="username">Username</label>
+            <input
+              onChange={this.handleChange}
+              value={this.state.account.username}
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
-          <div className="form-group"><label htmlFor="password">Password</label>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
             <input id="password" type="text" className="form-control"/>
           </div>
           <button className="btn btn-primary">Login</button>
