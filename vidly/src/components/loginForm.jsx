49afe4ca6ebb2ee89afac817a2e552d0d02a8410
weite122 <949193531@qq.com>
2019-09-03 21:25:39 +0800

    [A]react中操作DOM

--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,13 +1,24 @@
 import React, {Component} from 'react';
 
 class LoginForm extends Component {
+  username = React.createRef()
+
+  // componentDidMount() {
+  //   this.username.current.focus()
+  // }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const username = this.username.current.value
+  }
+
   render() {
     return (
       <div>
         <h1>login</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <div className="form-group"><label htmlFor="username">Username</label>
-            <input id="username" type="text" className="form-control"/>
+            <input autoFocus ref={this.username} id="username" type="text" className="form-control"/>
           </div>
           <div className="form-group"><label htmlFor="password">Password</label>
             <input id="password" type="text" className="form-control"/>
