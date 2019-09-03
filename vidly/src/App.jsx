cfc58dd0ae81501a06461ae09f0adbcf945ffec0
weite122 <949193531@qq.com>
2019-09-03 12:21:06 +0800

    [A]添加navbar

--- vidly/src/App.jsx
+++ vidly/src/App.jsx
@@ -4,22 +4,25 @@ import Movies from "./components/movies";
 import NotFound from "./components/NotFound";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
+import NavBar from "./components/navbar";
 import "./App.css";
-
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies} />
-          <Route path="/customers" component={Customers}/>
-          <Route path="/rentals" component={Rentals}/>
-          <Route path="/not-found" component={NotFound}/>
-          <Redirect from="/" exact to="/movies"/>
-          <Redirect to="/not-found"/>
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers}/>
+            <Route path="/rentals" component={Rentals}/>
+            <Route path="/not-found" component={NotFound}/>
+            <Redirect from="/" exact to="/movies"/>
+            <Redirect to="/not-found"/>
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
--- vidly/src/components/navbar.jsx
+++ vidly/src/components/navbar.jsx
@@ -1,15 +1,36 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-import React from "react";
+import React from 'react';
+import {Link, NavLink} from "react-router-dom";
 
-const NavBar = ({totalCounters}) => {
+const NavBar = () => {
   return (
-    <nav className="navbar nav-light bg-light">
-      <a className="navbar-brand" href="#">
-        NavBar{" "}
-        <span className="badge badge-pill badge-secondary">
-          {totalCounters}
-        </span>
-      </a>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        Vidly
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon" />
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink className="nav-item nav-link" to="/movies">
+            Movies
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/customers">
+            Customers
+          </NavLink>
+          <NavLink className="nav-item nav-link" to="/rentals">
+            Rentals
+          </NavLink>
+        </div>
+      </div>
     </nav>
   );
 };
