2524b8600a4548c4f1b54b4967b46c3f78b35f12
weite122 <949193531@qq.com>
2019-09-04 04:11:04 +0800

    [A]封装input组件

---
+++ vidly/src/components/common/input.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Input = ({name, label, value, onChange}) => {
+  return (
+    <div className="form-group">
+      <label htmlFor="name">{label}</label>
+      <input
+        onChange={onChange}
+        value={value}
+        name="name"
+        autoFocus
+        id="name"
+        type="text"
+        className="form-control"
+      />
+    </div>
+  );
+};
+
+export default Input;
--- vidly/src/components/loginForm.jsx
+++ vidly/src/components/loginForm.jsx
@@ -1,49 +1,42 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import Input from "./common/input";
 
 class LoginForm extends Component {
   state = {
     account: {
-      username: '',
-      password: ''
+      username: "",
+      password: ""
     }
-  }
+    //表单创建时state的内容要么是空字符串，或者是从服务器获取的数据
+  };
   handleSubmit = e => {
     e.preventDefault();
   };
-  handleChange = ({currentTarget: input}) => {
-    const account = {...this.state.account}
-    account[input.name] = input.value
-    this.setState({account})
-  }
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
 
   render() {
-    const {account} =this.state
+    const { account } = this.state;
 
     return (
       <div>
         <h1>login</h1>
         <form onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <input
-              onChange={this.handleChange}
-              value={account.username}
-              name="username"
-              autoFocus
-              ref={this.username}
-              id="username"
-              type="text"
-              className="form-control"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input
-              onChange={this.handleChange}
-              value={account.password}
-              name="password"
-              id="password" type="text" className="form-control"/>
-          </div>
+          <Input
+            name="username"
+            value={account.username}
+            label="Username"
+            onChange={this.handleChange}
+          />
+          <Input
+            name="password"
+            value={account.password}
+            label="Password"
+            onChange={this.handleChange}
+          />
           <button className="btn btn-primary">Login</button>
         </form>
       </div>
